4fadc0841c4ef76279d8672fd14a1792b48dbf46
orlandojtx <46915982+orlandojtx@users.noreply.github.com>
2021-10-28 21:27:18 -0500

    update deploy be

--- covid_passport/settings.py
+++ covid_passport/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$^w7$d%!5%d(i&(xs8hfhzyi5fo_xe-ataq!!u3-h8ca#^vi#t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://pasaporte-covid-fe.herokuapp.com/#/']
 
 
 # Application definition
